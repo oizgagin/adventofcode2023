3183be3c364c2b08da19dcfecc3b2b459d82b16e
Oleg Izgagin <37784140+oizgagin@users.noreply.github.com>
2023-12-09 11:39:30 +0100

    add 9.1

---
+++ 09/1.py
@@ -0,0 +1,22 @@
+def solution(histories):
+
+    def predict(history):
+        if all(v == 0 for v in history):
+            return 0
+
+        diffs = [history[i] - history[i-1] for i in xrange(1, len(history))]
+        return history[-1] + predict(diffs)
+
+    return sum(predict(history) for history in histories)
+
+
+def parse(input):
+    return map(lambda s: map(int, s.split()), input.splitlines())
+
+
+def main():
+    input = open("input", "r").read()
+    print(solution(parse(input)))
+
+
+main()
